f6fd8191fa82fd23a7c365dd2b4469127887a0c3
thitithup <thitithup@gmail.com>
2013-05-29 09:31:28 +0000

    Add releate Garment Order No in stock.picking.out

--- ineco_quotation_garment/stock.py
+++ ineco_quotation_garment/stock.py
@@ -64,6 +64,7 @@ class stock_picking_out(osv.osv):
         'shiping_cost': fields.float('Shipping Cost', digits_compute=dp.get_precision('Account')),
         'batch_no': fields.integer('Batch No'),
         'ineco_date_delivery': fields.date('Actual Delivery Date'),
+        'garment_order_no': fields.related('sale_id', 'garment_order_no', type="char", string="Garment No", readonly=True),        
     }
         
     
