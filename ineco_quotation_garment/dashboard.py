98c6822643d214556ac23a23b1cfabf3e3a36aac
thitithup <thitithup@gmail.com>
2014-01-21 03:37:28 +0000

    Add Sale Summary 2

--- ineco_quotation_garment/dashboard.py
+++ ineco_quotation_garment/dashboard.py
@@ -230,3 +230,154 @@ class ineco_sale_summary4(osv.osv):
                 ) c
             )""")
         
+class ineco_sale_summary5_query(osv.osv):
+    _name = "ineco.sale.summary5.query"
+    _auto = False
+    
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'ineco_sale_summary5_query')
+        cr.execute("""
+        CREATE OR REPLACE VIEW ineco_sale_summary5_query AS (
+                select 
+                  ru.id as user_id,
+                  ru.nickname,
+                  /*(select count(*) from sale_order so 
+                   where user_id = ru.id and date_part('month',now()) = date_part('month',so.date_sale_close)
+                         and so.state <> 'cancel'
+                  ) as so1,*/
+                  (select coalesce(sum(amount_untaxed),0) from sale_order so 
+                   where user_id = ru.id and date_part('month',now()) = date_part('month',so.date_sale_close)
+                         and so.state <> 'cancel'
+                  ) as so2,
+                  /*
+                  (select count(*)::numeric  from sale_order so 
+                   where user_id = ru.id and date_part('month',now()) = date_part('month',so.garment_order_date)
+                         and so.state <> 'cancel'
+                  ) as mo1,*/
+                  (select coalesce(sum(amount_untaxed),0) from sale_order so 
+                   where user_id = ru.id and date_part('month',now()) = date_part('month',so.garment_order_date)
+                         and so.state <> 'cancel'
+                  ) as mo2,
+                  (
+                    select coalesce(count(*),0) from res_partner 
+                    where user_id = ru.id
+                  ) as total_customer,
+                  (
+                    select coalesce(count(*),0) from res_partner 
+                    where user_id = ru.id and is_company = true
+                  ) as total_customer_company,            
+                  (
+                    select coalesce(count(*),0) from res_partner 
+                    where user_id = ru.id and date_part('month',now()) = date_part('month', res_partner.create_date)
+                        and date_part('year',now()) = date_part('year', res_partner.create_date)
+                  ) as total_customer_new,
+                  (select coalesce(count(*),0) from crm_phonecall cp
+            left join crm_case_categ ccc on cp.categ_id = ccc.id
+            where ccc.id = 9 and date_part('month',now()) = date_part('month', cp.create_date)
+                        and date_part('year',now()) = date_part('year', cp.create_date)  
+                        and cp.user_id = ru.id
+                  ) as logcall_inbound,
+                  (select coalesce(count(*),0) from crm_phonecall cp
+            left join crm_case_categ ccc on cp.categ_id = ccc.id
+            where ccc.id = 10  and date_part('month',now()) = date_part('month', cp.create_date)
+                        and date_part('year',now()) = date_part('year', cp.create_date)  
+                        and cp.user_id = ru.id
+                  ) as logcall_outbound,
+                  (select coalesce(count(*),0) from crm_phonecall cp
+            left join crm_case_categ ccc on cp.categ_id = ccc.id
+            where cp.visit = true and date_part('month',now()) = date_part('month', cp.create_date)
+                        and date_part('year',now()) = date_part('year', cp.create_date)  
+                        and cp.user_id = ru.id
+                  ) as logcall_visit,
+                  (
+                    select count(*) from sale_order where state <> 'cancel'  and user_id = ru.id
+                        and date_part('month',now()) = date_part('month', sale_order.create_date)
+                        and date_part('year',now()) = date_part('year', sale_order.create_date)  
+                  ) as total_quotation,
+                  (
+                    select count(*) from sale_order where state in ('manual','send')  and user_id = ru.id
+                        and date_part('month',now()) = date_part('month', sale_order.create_date)
+                        and date_part('year',now()) = date_part('year', sale_order.create_date)  
+                  ) as total_quotation_saleorder,
+                  (
+                    select count(*) from stock_picking sp
+                  where stock_journal_id = 1 and state = 'done' and sp.create_uid = ru.id
+                  and date_part('month',now()) = date_part('month', sp.create_date)
+                         and date_part('year',now()) = date_part('year', sp.create_date)  
+                  ) as total_picking_pc,
+                  (
+                    select count(*) from stock_picking sp
+                  where stock_journal_id = 2 and state = 'done' and sp.create_uid = ru.id
+                  and date_part('month',now()) = date_part('month', sp.create_date)
+                         and date_part('year',now()) = date_part('year', sp.create_date)  
+                  ) as total_picking_ds,
+                  (
+                    select count(*) from stock_picking sp
+                  where stock_journal_id = 3 and state = 'done' and sp.create_uid = ru.id
+                  and date_part('month',now()) = date_part('month', sp.create_date)
+                         and date_part('year',now()) = date_part('year', sp.create_date)  
+                  ) as total_picking_rp,
+                  (
+                    select count(*) from stock_picking sp
+                  where stock_journal_id = 4 and state = 'done' and sp.create_uid = ru.id
+                  and date_part('month',now()) = date_part('month', sp.create_date)
+                         and date_part('year',now()) = date_part('year', sp.create_date)  
+                  ) as total_picking_mp,
+                  (
+                    select count(*) from stock_picking sp
+                  where stock_journal_id = 5 and state = 'done' and sp.create_uid = ru.id
+                  and date_part('month',now()) = date_part('month', sp.create_date)
+                         and date_part('year',now()) = date_part('year', sp.create_date)  
+                  ) as total_picking_fr
+                from 
+                  res_users ru
+                left join res_partner rp on ru.partner_id = rp.id
+                where ru.active = true and ru.id not in (70,71,72,23,16,61,20,1,18,22,21,66,60) and
+                   signature like '%เจ้าหน้าที่งานฝ่ายขาย%'
+                order by rp.name      
+        )    
+        """)
+        
+class ineco_sale_summary5(osv.osv):
+    _name = 'ineco.sale.summary5'
+    _auto = False
+    _columns = {
+        'user_id': fields.many2one('res.users', 'Sale'),
+        'nickname': fields.char('Nick Name', size=32),
+        'so2': fields.integer('SO',),
+        'mo2': fields.integer('MO',),
+        'total_customer': fields.integer('Total',),
+        'total_customer_company': fields.integer('Company',),
+        'total_customer_new': fields.integer('New',),
+        'logcall_inbound': fields.integer('In',),
+        'logcall_outbound': fields.integer('Out',),
+        'logcall_visit': fields.integer('Visit',),
+        'total_quotation': fields.integer('QO',),
+        'total_quotation_saleorder': fields.integer('SO',),
+        'total_picking_pc': fields.integer('PC',),
+        'total_picking_ds': fields.integer('DS',),
+        'total_picking_rp': fields.integer('PR',),
+        'total_picking_mp': fields.integer('PM',),
+        'total_picking_fr': fields.integer('PF',),
+    }     
+    
+    def init(self, cr):
+
+        """
+            CRM Lead Report
+            @param cr: the current row, from the database cursor
+        """
+        tools.drop_view_if_exists(cr, 'ineco_sale_summary5')
+        cr.execute("""
+            CREATE OR REPLACE VIEW ineco_sale_summary5 AS (
+                select id, (a[id]).*
+                from (
+                    select a, generate_series(1, array_upper(a,1)) as id
+                        from (
+                            select array (
+                                select ineco_sale_summary5_query from ineco_sale_summary5_query
+
+                            ) as a
+                    ) b
+                ) c
+            )""")        
