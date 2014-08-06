a96d9fed56b33b5d57745e65a716afccca480004
thitithup <thitithup@gmail.com>
2014-08-06 06:59:09 +0000

    Change pattern kanban

--- ineco_quotation_garment/pattern.py
+++ ineco_quotation_garment/pattern.py
@@ -21,9 +21,19 @@
 
 from openerp.osv import osv, fields
 import time
+from openerp import tools
 #import openerp.addons.decimal_precision as dp
 
 class ineco_pattern(osv.osv):
+
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
         
     _name = 'ineco.pattern'
     _inherit = ['mail.thread']
@@ -47,6 +57,22 @@ class ineco_pattern(osv.osv):
         'active': fields.boolean('Active'),
         'image': fields.binary('Image'),
         'multi_images': fields.text("Multi Images"),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'fleet.vehicle.model.brand': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Medium-sized logo of the brand. It is automatically "\
+                 "resized as a 128x128px image, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small': fields.function(_get_image, fnct_inv=_set_image,
+            string="Smal-sized photo", type="binary", multi="_get_image",
+            store = {
+                'fleet.vehicle.model.brand': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Small-sized photo of the brand. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image is required."),
     }
     
     _sql_constraints = [
