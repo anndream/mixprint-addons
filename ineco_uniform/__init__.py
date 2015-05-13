f977afe725d0185f48d6f6d6f9a70d8db1e049d5
thitithup <thitithup@gmail.com>
2015-05-13 04:06:23 +0000

    Change requirement

--- ineco_uniform/__init__.py
+++ ineco_uniform/__init__.py
@@ -20,6 +20,6 @@
 ##############################################################################
 
 import sale
-#import dashboard
+import dashboard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
